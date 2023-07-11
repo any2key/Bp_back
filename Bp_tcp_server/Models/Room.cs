e86677354d849ebdc00791dbb2f7737ccd679e48
any2key <any2key@yandex.ru>
2023-07-11 23:44:32 +0300

    bp_hub =>web api

--- Bp_tcp_server/Models/Room.cs
+++ Bp_tcp_server/Models/Room.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Bp_tcp_server.Client;
-namespace Bp_tcp_server.Client;
 
 internal class Room
 {
     internal Guid Id { get; set; }
-    internal List<Client> Clients { get; set; }
 }
--- Bp_tcp_server/Server/BpServer.cs
+++ Bp_tcp_server/Server/BpServer.cs
@@ -15,11 +15,36 @@ namespace Bp_tcp_server.Server
         private IBpLogger logger;
         private TcpListener tcpListener;
         private readonly IBpConfiguration config;
+
+        private List<Connection> connections = new List<Connection>();
         public BpServer(IBpLogger logger, IBpConfiguration config)
         {
             this.logger = logger;
             this.config = config;
         }
+
+
+        protected internal void RemoveConnection(string id)
+        {
+            // получаем по id закрытое подключение
+            Connection? connection = connections.FirstOrDefault(c => c.Id == id);
+            // и удаляем его из списка подключений
+            if (connection != null) connections.Remove(connection);
+            connection?.Close();
+        }
+
+        protected internal async Task BroadcastMessageAsync(string message, string id)
+        {
+            foreach (var connection in connections)
+            {
+                if (connection.Id != id) // если id клиента не равно id отправителя
+                {
+                    await connection.Writer.WriteLineAsync(message); //передача данных
+                    await connection.Writer.FlushAsync();
+                }
+            }
+        }
+
 
         async Task IBbServer.Start()
         {
@@ -29,19 +54,99 @@ namespace Bp_tcp_server.Server
                 tcpListener = new TcpListener(IPAddress.Any, config.Port);
                 tcpListener.Start();
                 logger.Log($"Server Started...");
-                
+
                 while (true)
                 {
                     TcpClient client = await tcpListener.AcceptTcpClientAsync();
-                    Task.Run(() => { });
+                    Connection connection = new Connection(client, this);
+                    connections.Add(connection);
+                    Task.Run(connection.ProcessAsync);
                 }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
             }
             finally
             {
-                tcpListener.Stop();
+                Disconnect();
             }
+
+        }
+
+        protected internal void Disconnect()
+        {
+            foreach (var connection in connections)
+            {
+                connection.Close(); //отключение клиента
+            }
+            tcpListener.Stop(); //остановка сервера
         }
 
 
+        class Connection
+        {
+            public string Id { get; set; }
+            protected internal StreamWriter Writer { get; }
+            protected internal StreamReader Reader { get; }
+
+            TcpClient client;
+            BpServer server;
+
+            public Connection(TcpClient client, BpServer server)
+            {
+                this.client = client;
+                this.server = server;
+
+                var stream = client.GetStream();
+                Reader = new StreamReader(stream);
+                Writer = new StreamWriter(stream);
+            }
+
+
+            public async Task ProcessAsync()
+            {
+                try
+                {
+                    string? message = await Reader.ReadLineAsync();
+                    await server.BroadcastMessageAsync(message, Id);
+                    Console.WriteLine(message);
+                    while (true)
+                    {
+                        try
+                        {
+                            message = await Reader.ReadLineAsync();
+                            if (message == null) continue;
+                            Console.WriteLine(message);
+                            await server.BroadcastMessageAsync(message, Id);
+                        }
+                        catch
+                        {
+                            message = $"Отключился";
+                            Console.WriteLine(message);
+                            await server.BroadcastMessageAsync(message, Id);
+                            break;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    // в случае выхода из цикла закрываем ресурсы
+                    server.RemoveConnection(Id);
+                }
+            }
+            protected internal void Close()
+            {
+                Writer.Close();
+                Reader.Close();
+                client.Close();
+            }
+
+
+        }
     }
 }
